5af72874f3dac71475272f86ef755fbefcf333fd
Eduardo Martins <eduardo.amaral382@gmail.com>
2023-09-25 18:27:40 -0300

    “commit”

---
+++ sorteio.py
@@ -0,0 +1,8 @@
+import random
+
+# 2 luciano, 3 hedria, 4 rosana, 5 Zé Adão, 6 marcilene,  9 breno, 12 alessandra, 13 breno, 14 verônica, 15 breno, 18 Pedro, 19 hedria, 20 rosilaine, 22 vitória/robson, 23 marcelo, 24 ieie,l 32 carlos, 43 Katiane, 57 Gil, 59 Júli,o 60 Carol, 72 Pedro, 76 Helen, 77 rosiane, 80 Hedria, 83 carol, 85 hellen, 92 breno, 98 carol, 99 breno.
+
+lista = [2, 3, 4, 5, 6, 9, 12, 13, 14, 15, 18, 19, 20, 22, 23,
+         24, 32, 43, 57, 59, 60, 72, 76, 77, 80, 83, 85, 92, 98, 99]
+
+print(random.choice(lista))
